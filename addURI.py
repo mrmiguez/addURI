6b51aa15daf4c3b694a3ae740788a450cefb2254
mrmiguez <r.m.miguez@gmail.com>
2016-09-29 13:21:32 -0400

    major parts functioning

--- addURI.py
+++ addURI.py
@@ -4,13 +4,56 @@ import requests
 import sys
 from lxml import etree
 
+def get_keyword_list(record):
+    keywords = []
+    for note in mods.note(record):
+        if 'Keywords' in note.keys():
+            for keyword in note['Keywords'].split(','):
+                keywords.append(keyword.strip())
+    return keywords
+
+class mods:
+
+    nameSpace_default = {'oai_dc': 'http://www.openarchives.org/OAI/2.0/oai_dc/', 
+                     'dc': 'http://purl.org/dc/elements/1.1/', 
+                     'mods': 'http://www.loc.gov/mods/v3', 
+                     'dcterms': 'http://purl.org/dc/terms'}
+
+
+    def load(input_file, nameSpace_dict=nameSpace_default):
+        record_list = []
+        tree = etree.parse(input_file)
+        root = tree.getroot()
+        for record in root.iterfind('.//{%s}mods' % nameSpace_dict['mods']):
+            record_list.append(record)
+        return record_list
+        
+    
+    def note(record, nameSpace_dict=nameSpace_default):
+        allNotes = []
+        for note in record.iterfind('./{%s}note' % nameSpace_dict['mods']):
+            if len(note.attrib) >= 1:
+                if 'type' in note.attrib.keys():
+                    typed_note = {note.attrib['type'] : note.text}
+                    allNotes.append(typed_note)
+                elif 'displayLabel' in note.attrib.keys():
+                    labeled_note = {note.attrib['displayLabel'] : note.text}
+                    allNotes.append(labeled_note)
+                else:
+                    allNotes.append(note.text)
+            else:
+                allANotes.append(note.text)
+        return allNotes    
+        
+
+
 class uri_lookup:
 
     #TGM
     def tgm(keyword):   
         keyword = keyword.replace(' ','%20')
         try:
-            tgm_lookup = requests.get('http://id.loc.gov/vocabulay/graphicMaterials/label/{0}'.format(keyword),
+            tgm_lookup = requests.get('http://id.loc.gov/vocabulary/graphicMaterials/label/{0}'.format(keyword),
                                         timeout=5)
             if tgm_lookup.status_code == 200:
                 print(tgm_lookup.url[0:-5])
@@ -39,5 +82,8 @@ class uri_lookup:
                 print(lcsh_lookup.status_code) 
         except requests.exceptions.Timeout:
             print('The request timed out after five seconds.')
-  
-uri_lookup.lcsh(sys.argv[1])
+          
+modsXML = etree.parse(sys.argv[1])
+modsRecord = modsXML.getroot()
+for keyword in get_keyword_list(modsRecord):
+    uri_lookup.lcsh(keyword)

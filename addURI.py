c6ee5b8eedd286d47a0c5c561136fe89adf3822c
mrmiguez <r.m.miguez@gmail.com>
2016-09-29 10:58:14 -0400

    server timeout handling

--- addURI.py
+++ addURI.py
@@ -9,30 +9,35 @@ class uri_lookup:
     #TGM
     def tgm(keyword):   
         keyword = keyword.replace(' ','%20')
-        tgm_lookup = requests.get('http://id.loc.gov/vocabulay/graphicMaterials/label/{0}'.format(keyword),
-                                    timeout=5)
-        if tgm_lookup.status_code == 200:
-            print(tgm_lookup.url[0:-5])
-        elif tgm_lookup.status_code == 404:
-            print('404: resource not found')
-        elif tgm_lookup.status_code == 503:
-            print(tgm_lookup.headers)
-        else:
-            print(tgm_lookup.status_code)
-   
+        try:
+            tgm_lookup = requests.get('http://id.loc.gov/vocabulay/graphicMaterials/label/{0}'.format(keyword),
+                                        timeout=5)
+            if tgm_lookup.status_code == 200:
+                print(tgm_lookup.url[0:-5])
+            elif tgm_lookup.status_code == 404:
+                print('404: resource not found')
+            elif tgm_lookup.status_code == 503:
+                print(tgm_lookup.headers)
+            else:
+                print(tgm_lookup.status_code)
+        except requests.exceptions.Timeout:
+            print('The request timed out after five seconds.')
 
    #LCSH
     def lcsh(keyword):
         keyword = keyword.replace(' ','%20')
-        lcsh_lookup = requests.get('http://id.loc.gov/authorities/subjects/label/{0}'.format(keyword),
+        try: 
+            lcsh_lookup = requests.get('http://id.loc.gov/authorities/subjects/label/{0}'.format(keyword),
                                     timeout=5)
-        if lcsh_lookup.status_code == 200:
-            print(lcsh_lookup.url[0:-5])
-        elif lcsh_lookup.status_code == 404:
-            print('404: resource not found')
-        elif lcsh_lookup.status_code == 503:
-            print(lcsh_lookup.headers)
-        else:
-            print(lcsh_lookup.status_code)                                
+            if lcsh_lookup.status_code == 200:
+                print(lcsh_lookup.url[0:-5])
+            elif lcsh_lookup.status_code == 404:
+                print('404: resource not found')
+            elif lcsh_lookup.status_code == 503:
+                print(lcsh_lookup.headers)
+            else:
+                print(lcsh_lookup.status_code) 
+        except requests.exceptions.Timeout:
+            print('The request timed out after five seconds.')
   
-uri_lookup.tgm(sys.argv[1])
+uri_lookup.lcsh(sys.argv[1])

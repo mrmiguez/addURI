bbca051bd4a8187901f2e13ad1b24b6d2535f4fa
mrmiguez <r.m.miguez@gmail.com>
2016-09-30 11:49:49 -0400

    more BS testing

--- bs4test.py
+++ bs4test.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 from lxml import etree
 from bs4 import BeautifulSoup
 
@@ -5,3 +7,6 @@ LCSH_complex_type_page = etree.parse(web_page_data, etree.HTMLParser)
 LCSH_complex_type_html = etree.tostring(LCSH_complex_type_page.getroot())
 LCSH_complex_type_soup = BeautifulSoup(LCSH_complex_type_html, 'lxml')
 LCSH_complex_type_components = LCSH_complex_type_soup.find_all("ul", rel="madsrdf:componentList")
+for heading in LCSH_complex_type_components[0].find_all('div'):
+    if 'madsrdf:Authority' in heading.get('typeof'):
+        print(heading.text)

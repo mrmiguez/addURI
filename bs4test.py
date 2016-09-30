fe11f361e965952993de5175e484c6b14ca54919
mrmiguez <r.m.miguez@gmail.com>
2016-09-30 15:11:20 -0400

    better functioning

--- bs4test.py
+++ bs4test.py
@@ -8,4 +8,4 @@ LCSH_complex_type_soup = BeautifulSoup(open(sys.argv[1]), 'lxml')
 for componentList in LCSH_complex_type_soup.find_all("ul", rel="madsrdf:componentList"):
     for heading in componentList.find_all('div'):
         if 'madsrdf:Authority' in heading.get('typeof'):
-            print(heading['typeof'].split(' ')[2], ' : ', heading.text.strip())
+            print(heading['typeof'].split(' ')[2], ':', heading.text.strip())

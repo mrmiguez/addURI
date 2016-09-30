a5e88b730c37d283d97600770fb3f7dfe5e858ff
mrmiguez <r.m.miguez@gmail.com>
2016-09-30 14:57:57 -0400

    beautiful soup pulling correct parts

--- bs4test.py
+++ bs4test.py
@@ -1,12 +1,11 @@
 #!/usr/bin/env python3
 
+import sys
 from lxml import etree
 from bs4 import BeautifulSoup
 
-LCSH_complex_type_page = etree.parse(web_page_data, etree.HTMLParser)
-LCSH_complex_type_html = etree.tostring(LCSH_complex_type_page.getroot())
-LCSH_complex_type_soup = BeautifulSoup(LCSH_complex_type_html, 'lxml')
-LCSH_complex_type_components = LCSH_complex_type_soup.find_all("ul", rel="madsrdf:componentList")
-for heading in LCSH_complex_type_components[0].find_all('div'):
-    if 'madsrdf:Authority' in heading.get('typeof'):
-        print(heading.text)
+LCSH_complex_type_soup = BeautifulSoup(open(sys.argv[1]), 'lxml')
+for componentList in LCSH_complex_type_soup.find_all("ul", rel="madsrdf:componentList"):
+    for heading in componentList.find_all('div'):
+        if 'madsrdf:Authority' in heading.get('typeof'):
+            print(heading['typeof'].split(' ')[2], ' : ', heading.text.strip())

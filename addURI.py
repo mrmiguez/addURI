68fcc85e2ac9ff723107f9adfa08407fccf6e728
mrmiguez <r.m.miguez@gmail.com>
2016-10-06 19:36:09 -0400

    broke some parts

--- addURI.py
+++ addURI.py
@@ -7,7 +7,7 @@ import logging
 import datetime
 import requests
 from lxml import etree
-
+from bs4 import BeautifulSoup
 
 nameSpace_default = {'oai_dc': 'http://www.openarchives.org/OAI/2.0/oai_dc/', 
                      'dc': 'http://purl.org/dc/elements/1.1/', 
@@ -16,17 +16,30 @@ nameSpace_default = {'oai_dc': 'http://www.openarchives.org/OAI/2.0/oai_dc/',
 LOC_try_index = 0                     
 error_log = False
 
+def get_subject_parts(subject_html):
+    subject_parts = {}
+    subject_soup = BeautifulSoup(subject_html.text, 'lxml')
+    for componentList in subject_soup.find_all("ul", rel="madsrdf:componentList"):
+        for heading in componentList.find_all('div'):
+            if 'madsrdf:Authority' in heading.get('typeof'):
+                subject_parts['part'] = heading['typeof'].split(' ')[2].split(':')[1]
+                subject_parts['heading'] = heading.text.strip()
+                subject_parts['URI'] = suject_html.url[0:-5]
+    return subject_parts
+
 def write_record_subjects(record, subjects, PID):
     with open('improvedMODS/' + PID.replace(':','_') + '.xml', 'w') as MODS_out:
         for appending_subject in subjects:
             if 'tgm' in appending_subject.keys():
                 subject = etree.Element('{%s}subject' % nameSpace_default['mods'],
-                                        authority='tgm', authorityURI='http://id.loc.gov/vocabulary/graphicMaterials')
-                topic = etree.SubElement(subject)
-                topic.text = appending_subject['tgm']
+                                        authority='tgm', 
+                                        authorityURI='http://id.loc.gov/vocabulary/graphicMaterials')
+                topic = etree.SubElement(subject, '{%s}topic' % nameSpace_default['mods'])
+                topic.text = appending_subject['tgm']['heading']
             elif 'lcsh' in appending_subject.keys():
                 subject = etree.Element('{%s}subject' % nameSpace_default['mods'],
-                                        authority='lcsh', authorityURI='http://id.loc.gov/authorities/subjects')
+                                        authority='lcsh', 
+                                        authorityURI='http://id.loc.gov/authorities/subjects')
                 subject.text = appending_subject['lcsh']
             record.append(subject)
         MODS_out.write(etree.tostring(record, pretty_print=True, xml_declaration=True, encoding="UTF-8").decode('utf-8'))
@@ -102,15 +115,15 @@ class uri_lookup:
 
     #TGM
     def tgm(keyword, record_PID):
-        pass
-    '''
+#        pass
+    
         global LOC_try_index
         global error_log
         tgm_lookup = requests.get('http://id.loc.gov/vocabulary/graphicMaterials/label/{0}'.format(keyword.replace(' ','%20')),
                                    timeout=5)
         if tgm_lookup.status_code == 200:
             LOC_try_index = 0
-            return tgm_lookup
+            return get_subject_parts(tgm_lookup)
         elif tgm_lookup.status_code == 404:
             logging.warning('404 - resource not found ; [{0}]--{1}'.format(record_PID, 'tgm:' + keyword))
             error_log = True
@@ -123,7 +136,7 @@ class uri_lookup:
             logging.warning('Other status code - {0} ; [{1}]--{2}'.format(tgm_lookup.status_code, record_PID, 'tgm:' + keyword))
             error_log = True
             return None
-    '''
+    
    #LCSH
     def lcsh(keyword, record_PID):
         pass
@@ -155,14 +168,14 @@ logging.basicConfig(filename='addURI_LOG{0}.txt'.format(datetime.date.today()),
                     datefmt='%m/%d/%Y %H:%M:%S %p')
 for record in mods.load(sys.argv[1]):
     record_write = True
-    appending_subjects = [{'tgm':'Frankenstein'},{'lcsh':'His wife'}]
+    appending_subjects = []
     while LOC_try_index <= 5:
         record_PID = mods.pid_search(record)
         print("Checking:", record_PID)
         for keyword in get_keyword_list(record):
             try:
                 if uri_lookup.tgm(keyword, record_PID) is not None:
-                    appending_subjects.append({'tgm': uri_lookup.tgm(keyword, record_PID)})
+                    appending_subjects.append({'tgm': uri_lookup.tgm(keyword, record_PID)}) #need heading & type
                     record_write = True
                 elif uri_lookup.lcsh(keyword, record_PID) is not None:
                     appending_subjects.append({'lcsh': uri_lookup.lcsh(keyword, record_PID)})
@@ -180,7 +193,8 @@ for record in mods.load(sys.argv[1]):
         if 'improvedMODS' not in os.listdir():
             os.mkdir('improvedMODS')
         print("Writing", record_PID)
-        write_record_subjects(record, appending_subjects, record_PID)
+        print(appending_subjects)
+ #       write_record_subjects(record, appending_subjects, record_PID)
 if error_log is True:
     print("\nSome keywords not found.\nDetails logged to: addURI_LOG{0}.txt\n".format(datetime.date.today()))
 

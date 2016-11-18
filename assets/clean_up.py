61bbdd4361ca359f01476ecfb9774a9e24476bdf
mrmiguez <r.m.miguez@gmail.com>
2016-11-18 17:52:15 -0500

    clean_up fixes

--- assets/clean_up.py
+++ assets/clean_up.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python3
+import os
 import sys
 
-def clean(in_file):
-    with open(in_file + 'out', 'w') as out_file:
-        for line in open(in_file, 'r'):
-            if 'mods:mods' in line:
-                out_file.write(line)
-            else:
-                out_file.write(line.replace('mods:',''))
+def clean(in_directory):
+    for in_file in os.listdir(in_directory):
+        with open(in_directory + '/' + in_file + 'out', 'w') as out_file:
+            for line in open(in_directory + '/' + in_file, 'r'):
+                if 'mods:mods' in line:
+                    out_file.write(line)
+                else:
+                    out_file.write(line.replace('mods:',''))
 
 clean(sys.argv[1])                

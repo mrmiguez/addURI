05fb7a2ca050fe16370cecf5cab9a40b6a5aa1e4
mrmiguez <r.m.miguez@gmail.com>
2016-12-01 10:49:44 -0500

    clarity improvements

---
+++ test.py
@@ -0,0 +1,10 @@
+#!/usr/env python3
+
+class uri_lookup:
+
+    def __init__(self, url, vocab):
+        self.url = url
+        self.vocab = vocab
+
+    def add(self, num):
+        self = num + 5
